8f82581ba4fe2c168161330e32b1cd05ab9a5c62
9raja1999 <draj29209@gmail.com>
2024-01-08 20:52:52 -0800

    User form hook for validation of forms

--- reactrouter/src/App.jsx
+++ reactrouter/src/App.jsx
@@ -1,15 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useMemo, useEffect } from 'react'
+import Child from './components/Child'
+import Login from './components/forms/Login'
 import './App.css'
-import Header from './components/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [randomNumber, setRandomNumber] = useState(null)
+  const [numbers, setNumber] = useState([1, 2, 3, 4, 5])
+
+  useEffect(() => {
+    console.log("Bla bla")
+  })
+
+
+  useEffect(() => {
+    console.log("MOUNTING")
+
+
+    return () => {
+      console.log("UNMOUNT")
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log("UPDATION")
+  }, [count])
+
+  const maxNumber = useMemo(() => {
+    console.log("CALCULATION IN PROGRESS")
+    return Math.max(...numbers) - count
+  }, [])
+
+  function handleCount() {
+    setCount(count + 1)
+  }
+
+  function handleRandomNumber() {
+    setRandomNumber(Math.random())
+  }
+
+
+  console.log("MAX", maxNumber)
+
+
 
   return (
     <>
-     <h1>Home</h1>
+      <h1>Home</h1>
+      <p>Count  {count}</p>
+
+      <button
+        onClick={handleCount}
+      >Click Count</button>
+      <Child text={"Child Components"} randomNumber={randomNumber} />
+      <button
+        onClick={handleRandomNumber}
+      >Generate Random</button>
+
+      <Login />
+      
     </>
   )
 }
